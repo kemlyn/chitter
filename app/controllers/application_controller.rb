e6063898b22e6429e57ec5b3630a129fcae10220
Kemlyn Reid <kemlyn@reel.tech>
2018-11-21 13:47:13 +0000

    sign-up and edit profie working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #call strong params
+  # call strong params
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #strong params
+  # strong params
   protected
-  	def configure_permitted_parameters
-  		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
-  		devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
-  		devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :remember_me)}
-  	end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :handle, :email, :password, :password_confirmation, :remember_me)}
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :handle, :email, :password, :password_confirmation, :remember_me)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :handle, :username, :current_password, :password, :password_confirmation, :remember_me)}
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,8 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:handle, :username, :email, :password, :password_confirmation])
-    puts 'BANG!'
+    configure_permitted_parameters
   end
 
   # If you have extra params to permit, append them to the sanitizer.
